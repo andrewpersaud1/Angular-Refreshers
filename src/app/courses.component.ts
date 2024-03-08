af9514d5d510ca5e1ba3cd4b37215af60f63ab42
andrewpersaud1 <andrewpersaud022@gmail.com>
2024-03-08 18:14:00 -0500

    event binding

--- src/app/courses.component.ts
+++ src/app/courses.component.ts
@@ -1,18 +1,12 @@
 import {Component} from '@angular/core';
-import { CoursesService } from './courses.service';
+
 
 @Component({
   selector:'courses',
   template: `
-        <img [src]="imageUrl" />
-        <table>
-          <tr>
-            <td [attr.colspan]="colspan"></td>
-          <tr>
-        </table>
+      <button [style.backgroundColor]="isActive ? 'blue': 'white' ">Save</button>
   `
 })
 export class CoursesComponent{
-  imageUrl="";
-  colspan=2;
+    isActive =false;
 }
